002eaa39d25cdfb83ce2e29b23e70b7ebbec7c97
Ethan <ethan@reluvate.com>
2023-05-27 17:41:52 +0800

    Add items to navbar and port APIs over to server

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,33 +1,39 @@
 import {signOut} from "firebase/auth";
-import {useAuth} from "../hooks/useAuth";
 import {auth} from "../firebase";
+
 import {useSignInWithGoogle} from "react-firebase-hooks/auth";
+import {useAuth} from "../hooks/useAuth";
 
 const Navbar = () => {
   const {playerProfile, user} = useAuth();
   const [signInWithGoogle] = useSignInWithGoogle(auth);
 
   return (
-    <div className="w-100">
-      <p>{playerProfile?.displayName}</p>
-      {user && (
-        <button
-          onClick={async () => {
-            await signInWithGoogle();
-          }}
-        >
-          Sign In
-        </button>
-      )}
-      {/* {user && !user.isAnonymous && ( */}
-      <button
-        onClick={async () => {
-          await signOut(auth);
-        }}
-      >
-        Sign Out
-      </button>
-      {/* )} */}
+    <div className="w-100 d-flex justify-content-evenly p-2">
+      <p>Floating Bridge</p>
+      <p>Lobby</p>
+      <p>Leaderboard</p>
+      <div>
+        <p>{playerProfile?.displayName}</p>
+        {user?.isAnonymous && (
+          <button
+            onClick={async () => {
+              await signInWithGoogle();
+            }}
+          >
+            Sign In
+          </button>
+        )}
+        {!user?.isAnonymous && (
+          <button
+            onClick={async () => {
+              await signOut(auth);
+            }}
+          >
+            Sign Out
+          </button>
+        )}
+      </div>
     </div>
   );
 };
