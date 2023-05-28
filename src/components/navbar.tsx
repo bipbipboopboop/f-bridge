1af97e7d7f469be1827ae32097bf57ff474bdb93
Ethan <ethanchen2009@hotmail.com>
2023-05-28 14:22:32 +0800

    Style navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,39 +4,81 @@ import {auth} from "../firebase";
 import {useSignInWithGoogle} from "react-firebase-hooks/auth";
 import {useAuth} from "../hooks/useAuth";
 import GreenButton from "./buttons/button.green";
-import OrangeButton from "./buttons/button.orange";
 
 const Navbar = () => {
   const {playerProfile, user} = useAuth();
   const [signInWithGoogle] = useSignInWithGoogle(auth);
 
   return (
-    <div className="w-100 d-flex justify-content-evenly p-2">
-      <p>Floating Bridge</p>
-      <p>Lobby</p>
-      <p>Leaderboard</p>
-      <div>
-        <p>{playerProfile?.displayName}</p>
-        {user?.isAnonymous && (
-          <OrangeButton
-            onClick={async () => {
-              await signInWithGoogle();
-            }}
-          >
-            Sign In
-          </OrangeButton>
-        )}
-        {!user?.isAnonymous && (
-          <button
-            onClick={async () => {
-              await signOut(auth);
-            }}
-          >
-            Sign Out
-          </button>
-        )}
+    <nav className="w-100 navbar navbar-expand-lg pt-5">
+      <a className="navbar-brand">Navbar</a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavDropdown"
+        aria-controls="navbarNavDropdown"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">
+              Home <span className="sr-only">(current)</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Features
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Pricing
+            </a>
+          </li>
+          {!user?.isAnonymous && (
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdownMenuLink"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                {playerProfile?.displayName}
+              </a>
+              <div
+                className="dropdown-menu"
+                aria-labelledby="navbarDropdownMenuLink"
+              >
+                <a className="dropdown-item">Edit Profile</a>
+                <a className="dropdown-item">Settings</a>
+                <button
+                  className="dropdown-item"
+                  onClick={async () => {
+                    await signOut(auth);
+                  }}
+                >
+                  Sign Out
+                </button>
+              </div>
+            </li>
+          )}
+          {user?.isAnonymous && (
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                {playerProfile?.displayName}
+              </a>
+            </li>
+          )}
+        </ul>
       </div>
-    </div>
+    </nav>
   );
 };
 
