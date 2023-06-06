99e00bdefa67d61199b95d816e76c7aa6fe9f0f0
Ethan <ethanchen2009@hotmail.com>
2023-06-06 23:30:25 +0800

    Fix misaligned cards in hand

--- src/components/gameroom/hand.tsx
+++ src/components/gameroom/hand.tsx
@@ -3,19 +3,13 @@ import "./hand.css";
 const Hand = () => {
   return (
     <div className="hand">
-      <PlayingCard card={{suit: "♠", value: 10, stringValue: "10"}} />
-      <PlayingCard
-        card={{suit: "♠", value: 10, stringValue: "10"}}
-        style={{marginLeft: "-2.2rem"}}
-      />
-      <PlayingCard
-        card={{suit: "♠", value: 10, stringValue: "10"}}
-        style={{marginLeft: "-2.2rem"}}
-      />
-      <PlayingCard
-        card={{suit: "♠", value: 10, stringValue: "10"}}
-        style={{marginLeft: "-2.2rem"}}
-      />
+      {/* <PlayingCard card={{suit: "♠", value: 10, stringValue: "10"}} /> */}
+      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].map(() => (
+        <PlayingCard
+          card={{suit: "♠", value: 10, stringValue: "10"}}
+          style={{marginLeft: "-2.2rem"}}
+        />
+      ))}
     </div>
   );
 };
