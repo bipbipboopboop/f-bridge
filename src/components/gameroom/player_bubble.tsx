a74bebb2f481774ed652050df98b70208daac7b7
Ethan <ethanchen2009@hotmail.com>
2023-06-09 02:10:43 +0800

    Design some random stuff for player_bubble

--- src/components/gameroom/player_bubble.tsx
+++ src/components/gameroom/player_bubble.tsx
@@ -5,44 +5,64 @@ import "./player_bubble.css";
 type Props = {
   player: GameRoomPlayer;
   currentPlayerIndex: 0 | 1 | 2 | 3;
+  location: "top" | "bottom" | "left" | "right";
 };
 
 const PlayerBubble = (props: Props) => {
-  const {player, currentPlayerIndex} = props;
+  const {player, currentPlayerIndex, location} = props;
   const isPlayerTurn = currentPlayerIndex === player.position;
   return (
-    <div className={`player-bubble-${isPlayerTurn ? "selected" : "waiting"}`}>
+    <div
+      className={`player-bubble-${location} ${
+        isPlayerTurn ? "selected" : "waiting"
+      }`}
+    >
+      <div className="d-flex justify-content-center">{player.displayName}</div>
       <div className="d-flex align-items-center">
         <img src={dino_sprite_1} />
-      </div>
-      <div className="d-flex justify-content-center">{player.displayName}</div>
-      <div
-        className="d-flex align-items-center justify-content-around"
-        style={{backgroundColor: "gray"}}
-      >
-        <div
-          className="d-flex align-items-center justify-content-center"
-          style={{
-            backgroundColor: "white",
-            color: "black",
-            height: "40px",
-            width: "30px",
-            borderRadius: "5px",
-          }}
-        >
-          <div>
-            <div>♣</div>
-            <div style={{color: "#e44145"}}>♦</div>
-          </div>
-          <div>
-            <div style={{color: "#e44145"}}>♥</div>
-            <div>♠</div>
-          </div>
-        </div>
-        {player.numCardsOnHand}
+        <PlayerNumCardHint numCardsOnHand={player.numCardsOnHand} />
       </div>
     </div>
   );
 };
 
 export default PlayerBubble;
+
+const PlayerNumCardHint = (props: {numCardsOnHand: number}) => {
+  return (
+    <div className="d-flex align-items-center justify-content-around">
+      <div
+        className="d-flex align-items-center justify-content-center"
+        style={{
+          backgroundColor: "white",
+          color: "black",
+          height: "40px",
+          width: "30px",
+          borderRadius: "5px",
+        }}
+      >
+        <div>
+          <div>♣</div>
+          <div
+            style={{
+              color: "#e44145",
+            }}
+          >
+            ♦
+          </div>
+        </div>
+        <div>
+          <div
+            style={{
+              color: "#e44145",
+            }}
+          >
+            ♥
+          </div>
+          <div>♠</div>
+        </div>
+      </div>
+      {props.numCardsOnHand}
+    </div>
+  );
+};
