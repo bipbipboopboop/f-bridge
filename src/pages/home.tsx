47a2c36d4326b46edda6267bea9d5a9f547b1a7e
Ethan <ethanchen2009@hotmail.com>
2023-06-09 01:25:29 +0800

    Add logo and home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,15 @@
-import React from "react";
-
-type Props = {};
-
-const Home = (props: Props) => {
-  return <div>Home</div>;
+import logo from "assets/logo.png";
+const Home = () => {
+  return (
+    <div className="w-100 h-100 d-flex">
+      <div className="w-50">
+        <img src={logo} className="h-100 w-100" />
+      </div>
+      <div className="w-50 h-100 d-flex flex-column align-items-center justify-content-center">
+        <h1 style={{fontSize: "5rem"}}>FOR DA BOIS</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
