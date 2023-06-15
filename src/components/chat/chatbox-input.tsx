52edecca88a239c10cfd00b7d7118b73ef71b50c
Ethan <ethanchen2009@hotmail.com>
2023-06-16 03:00:34 +0800

    Add button to bidding-options-panel and fix chat scroll bug

--- src/components/chat/chatbox-input.tsx
+++ src/components/chat/chatbox-input.tsx
@@ -1,4 +1,4 @@
-import {memo, useState} from "react";
+import {ChangeEvent, FormEvent, memo, useState} from "react";
 import GreenButton from "../buttons/button-green";
 import useFunctions from "../../hooks/useFunctions";
 import {useParams} from "react-router-dom";
@@ -10,26 +10,25 @@ const ChatboxInput = () => {
 
   console.log("ChatboxInput loaded");
   if (!roomID) return <></>;
+
+  const onChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputMessage(e.target.value);
+  };
+
+  const onSendMessage = async (e: FormEvent) => {
+    e.preventDefault();
+    await sendMessage({roomID, message: inputMessage});
+    console.log("Sent");
+    setInputMessage("");
+  };
+
   return (
-    <div className="chatbox-bottom">
-      <input
-        type="text"
-        className="textbox"
-        value={inputMessage}
-        onChange={(e) => {
-          setInputMessage(e.target.value);
-        }}
-      />
-      <GreenButton
-        onClick={async () => {
-          await sendMessage({roomID, message: inputMessage});
-          console.log("Sent");
-          setInputMessage("");
-        }}
-      >
+    <form className="chatbox-bottom" onSubmit={onSendMessage}>
+      <input type="text" className="textbox" value={inputMessage} onChange={onChangeInput} />
+      <GreenButton type="submit" disabled={!inputMessage}>
         Send
       </GreenButton>
-    </div>
+    </form>
   );
 };
 
