db432107fc6114da89d7d8b1a1b7ff18910d33da
Ethan <ethanchen2009@hotmail.com>
2023-07-03 01:02:26 +0800

    Checkstyle

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,72 +1,9 @@
-import {Dispatch, SetStateAction, useState} from "react";
-import {useAuth} from "../hooks/useAuth";
+import React from "react";
 
-import PlayingCard from "assets/playing_card";
-import {Card, Suit} from "types/Card";
-import BidButton from "../components/buttons/button-bid";
-import {deck} from "../utils/deck";
+type Props = {};
 
-const Test = () => {
-  const [selectedSuit, setSelectedSuit] = useState<Suit | null>(null);
-  const [selectedCard, setSelectedCard] = useState<Card | null>(null);
-
-  return (
-    <div className="w-100 h-100 d-flex flex-column align-items-center" style={{border: "1px solid white"}}>
-      <div className="h-50 d-flex align-items-end">{selectedCard && <PlayingCard card={selectedCard} />}</div>
-      <div className="h-50 w-100 d-flex flex-column">
-        {["♣", "♦", "♥", "♠"].map((suit) => (
-          <div className="d-flex my-2" key={suit}>
-            <BidButton
-              style={{marginRight: "0.6rem", color: `${suit === "♣" || suit === "♠" ? "black" : "red"}`}}
-              onClick={() => {
-                setSelectedSuit(suit as Suit);
-              }}
-            >
-              {suit}
-            </BidButton>
-            <OtherCardsComponent suit={suit as Suit} selectedSuit={selectedSuit} setSelectedCard={setSelectedCard} />
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+const Test = (props: Props) => {
+  return <div>Test</div>;
 };
+
 export default Test;
-
-const OtherCardsComponent = (props: {
-  suit: Suit;
-  selectedSuit: Suit | null;
-  setSelectedCard: Dispatch<SetStateAction<Card | null>>;
-}) => {
-  const {gamePlayer} = useAuth();
-  const {suit, selectedSuit, setSelectedCard} = props;
-  if (!gamePlayer) return <></>;
-  if (!selectedSuit) return <></>;
-
-  const otherCardsLookup: Record<Suit, Card[]> = {
-    "♣": deck
-      .filter((card) => card.suit === "♣")
-      .filter((card) => !gamePlayer.cards.some((c) => c.stringValue === card.stringValue && c.suit === card.suit)),
-    "♦": deck
-      .filter((card) => card.suit === "♦")
-      .filter((card) => !gamePlayer.cards.some((c) => c.stringValue === card.stringValue && c.suit === card.suit)),
-    "♥": deck
-      .filter((card) => card.suit === "♥")
-      .filter((card) => !gamePlayer.cards.some((c) => c.stringValue === card.stringValue && c.suit === card.suit)),
-    "♠": deck
-      .filter((card) => card.suit === "♠")
-      .filter((card) => !gamePlayer.cards.some((c) => c.stringValue === card.stringValue && c.suit === card.suit)),
-  };
-
-  if (!(suit === selectedSuit)) return <></>;
-
-  return (
-    <div className="d-flex">
-      {otherCardsLookup[selectedSuit].map((card) => (
-        <BidButton key={`${card.suit} - ${card.stringValue}`} onClick={() => setSelectedCard(card)}>
-          {card.stringValue}
-        </BidButton>
-      ))}
-    </div>
-  );
-};
--- src/pages/game-board.tsx
+++ src/pages/game-board.tsx
@@ -24,165 +24,12 @@ const GameBoard = () => {
   /**
    * GameState
    */
-  const gameStateRef = doc(firestore, "gameRooms", roomID || "ERROR") as DocumentReference<GameState>;
+  const gameStateRef = (roomID && (doc(firestore, "gameRooms", roomID) as DocumentReference<GameState>)) || null;
   const [gameState, isLoading, error] = useDocumentData<GameState>(gameStateRef);
 
   /**
    * DEVELOPMENT TESTING
    */
-
-  // const gameState: GameState = {
-  //   createdAt: new Date(),
-  //   settings: {
-  //     isSpectatorAllowed: false,
-  //     isInviteOnly: false,
-  //   },
-  //   invitedID: [],
-  //   hostID: "4bTcgc3TyJ7oxb7QlYHYFiAI8X1E",
-  //   trickTakingPhase: null,
-  //   roomID: "An28rz5Jc",
-  //   players: [
-  //     {
-  //       country: "International",
-  //       numOfGamesPlayed: 0,
-  //       isHost: true,
-  //       numOfGamesWon: 0,
-  //       avatarID: null,
-  //       displayName: "other-tan",
-  //       isReady: true,
-  //       id: "4bTcgc3TyJ7oxb7QlYHYFiAI8X1E",
-  //       position: 0,
-  //       email: null,
-  //       roomID: "An28rz5Jc",
-  //     },
-  //     {
-  //       country: "International",
-  //       numOfGamesPlayed: 0,
-  //       isHost: false,
-  //       numOfGamesWon: 0,
-  //       avatarID: null,
-  //       displayName: "mental-amaranth",
-  //       isReady: true,
-  //       id: "LuqFI9HmssfllRzRBwe4l1LzPQ73",
-  //       position: 1,
-  //       email: null,
-  //       roomID: null,
-  //     },
-  //     {
-  //       country: "International",
-  //       numOfGamesPlayed: 0,
-  //       isHost: false,
-  //       numOfGamesWon: 0,
-  //       avatarID: null,
-  //       displayName: "grumpy-purple",
-  //       isReady: true,
-  //       id: "5nbM6VFyy9FLZpSOEQlc18znR2jY",
-  //       position: 2,
-  //       email: null,
-  //       roomID: null,
-  //     },
-  //     {
-  //       country: "International",
-  //       numOfGamesPlayed: 0,
-  //       isHost: false,
-  //       numOfGamesWon: 0,
-  //       avatarID: null,
-  //       displayName: "awful-blush",
-  //       isReady: true,
-  //       id: "SJfR7VXcVQBuMejoI8KqwnslvVXP",
-  //       position: 3,
-  //       email: null,
-  //       roomID: null,
-  //     },
-  //   ],
-  //   biddingPhase: {
-  //     currentPlayerIndex: 0,
-  //     highestBid: null,
-  //     bidHistory: [
-  //       {
-  //         p0: {
-  //           bid: null,
-  //           info: {
-  //             displayName: "Player 1",
-  //             id: "1",
-  //           },
-  //         },
-  //         p1: {
-  //           bid: null,
-  //           info: {
-  //             displayName: "Player 2",
-  //             id: "2",
-  //           },
-  //         },
-  //         p2: {
-  //           bid: null,
-  //           info: {
-  //             displayName: "Player 3",
-  //             id: "3",
-  //           },
-  //         },
-  //         p3: {
-  //           bid: null,
-  //           info: {
-  //             displayName: "Player 4",
-  //             id: "4",
-  //           },
-  //         },
-  //       },
-  //     ],
-  //     numPasses: 0,
-  //     gameroomPlayersList: [
-  //       {
-  //         numCardsOnHand: 13,
-  //         numTricksWon: 0,
-  //         avatarID: null,
-  //         displayName: "other-tan",
-  //         currentCardOnTable: null,
-  //         id: "4bTcgc3TyJ7oxb7QlYHYFiAI8X1E",
-  //         position: 0,
-  //       },
-  //       {
-  //         numCardsOnHand: 13,
-  //         numTricksWon: 0,
-  //         avatarID: null,
-  //         displayName: "mental-amaranth",
-  //         currentCardOnTable: null,
-  //         id: "LuqFI9HmssfllRzRBwe4l1LzPQ73",
-  //         position: 1,
-  //       },
-  //       {
-  //         numCardsOnHand: 13,
-  //         numTricksWon: 0,
-  //         avatarID: null,
-  //         displayName: "grumpy-purple",
-  //         currentCardOnTable: null,
-  //         id: "5nbM6VFyy9FLZpSOEQlc18znR2jY",
-  //         position: 2,
-  //       },
-  //       {
-  //         numCardsOnHand: 13,
-  //         numTricksWon: 0,
-  //         avatarID: null,
-  //         displayName: "awful-blush",
-  //         currentCardOnTable: null,
-  //         id: "SJfR7VXcVQBuMejoI8KqwnslvVXP",
-  //         position: 3,
-  //       },
-  //     ],
-  //   },
-  //   status: "Bidding",
-  // };
-
-  // const playerProfile = {
-  //   country: "International",
-  //   numOfGamesPlayed: 0,
-  //   numOfGamesWon: 0,
-  //   avatarID: null,
-  //   displayName: "other-tan",
-  //   id: "4bTcgc3TyJ7oxb7QlYHYFiAI8X1E",
-  //   email: null,
-  //   roomID: "An28rz5Jc",
-  // };
 
   // const isLoading = false;
   // const error = null;
@@ -190,10 +37,11 @@ const GameBoard = () => {
   console.log({ playerProfile, gameState });
 
   if (isLoading) return <Loading />;
+
   if (!gameState) {
     toast.error("You are not allowed in this room!");
     navigate("/lobby");
-    return <></>;
+    return <>404</>;
   }
 
   const isPlayerAllowedIn =
@@ -209,7 +57,8 @@ const GameBoard = () => {
     toast.error("You are not in this room!");
     return <>404</>;
   }
-  if (gameState.status === "Waiting") return <>404</>;
+
+  if (!["Bidding", "Choosing Teammate", "Taking Trick"].includes(gameState.status)) return <>404</>;
 
   return (
     <div className="game-component">
