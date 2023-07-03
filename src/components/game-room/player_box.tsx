de4997f859825bd796f3b33ba8b610767502afc4
Ethan <ethanchen2009@hotmail.com>
2023-07-04 01:55:44 +0800

    Add highlight to player's own profile in playerbox

--- src/components/game-room/player_box.tsx
+++ src/components/game-room/player_box.tsx
@@ -1,16 +1,18 @@
 import { LobbyPlayerProfile } from "types/PlayerProfile";
 import "./player_box.css";
 import { avatarLookup } from "assets/avatar";
+import { useAuth } from "../../hooks/useAuth";
 
 const PlayerBox = ({ player }: { player: LobbyPlayerProfile | undefined }) => {
+  const { playerProfile } = useAuth();
   if (!player) return <div className="player-box"></div>;
-  // Convert player avatarID to sprite index
 
+  const isMe = playerProfile?.id === player.id;
   return (
     <div className="player-box">
       <div>
         <span style={{ fontSize: "1.5rem" }}>{player?.isHost ? "👑" : ""}</span>
-        <span style={{ fontSize: "0.5rem" }}>{player?.displayName}</span>
+        <span style={{ fontSize: "0.5rem", color: isMe ? "yellow" : "white" }}>{player?.displayName}</span>
       </div>
       <img style={{ height: "6rem" }} src={avatarLookup[player.avatarID]} />
       <span>{`${player?.isReady ? "Ready" : "Not Ready"}`}</span>
