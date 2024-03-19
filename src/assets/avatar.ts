80628b7d37913b232ca43ad7ebd7c4d7ec82d94d
Ethan <ethanchen2009@hotmail.com>
2024-03-20 04:32:27 +0800

    Delete redundant files

--- src/assets/avatar.ts
+++ src/assets/avatar.ts
@@ -2,11 +2,11 @@
  * This contains exports of all the avatars
  */
 
+import { AvatarID } from "types/Avatar";
 import BLUE_DINO from "./player_assets/dino_sprite_1.gif";
 import RED_DINO from "./player_assets/dino_sprite_2.gif";
 import YELLOW_DINO from "./player_assets/dino_sprite_3.gif";
 import GREEN_DINO from "./player_assets/dino_sprite_4.gif";
-import { AvatarID } from "types/Avatar";
 
 // avatarLookup maps AvatarID to the corresponding image, the keys are the AvatarID and the values are the image paths
 export const avatarLookup: Record<AvatarID, string> = {
