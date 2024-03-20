47197c93e2f4a85e10669e2295158cd263631dee
Ethan <ethanchen2009@hotmail.com>
2024-03-20 16:02:25 +0800

    Add BiddingRoom

--- src/components/room/bidding/BiddingRoom.tsx
+++ src/components/room/bidding/BiddingRoom.tsx
@@ -1,9 +1,18 @@
-import React from "react";
+import { GameRoom } from "types/Room";
+import Chatbox from "../../chat/Chatbox";
+import Navbar from "../../navbar";
 
-type Props = {};
-
-const BiddingRoom = (props: Props) => {
-  return <div>BiddingRoom</div>;
+const BiddingRoom = ({ room }: { room: GameRoom }) => {
+  return (
+    <div className="h-full w-full">
+      <div className="flex w-full h-full px-10">
+        <div className="h-full w-2/3">Bidding</div>
+        <div className="h-full w-1/3 p-4">
+          <Chatbox />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BiddingRoom;
