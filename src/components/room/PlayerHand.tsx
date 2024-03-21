1d737ff0ce2ae6e51631cd61f4ceb627b5727652
Ethan <ethanchen2009@hotmail.com>
2024-03-21 20:26:48 +0800

    Add sorting feature to PlayerHand

--- src/components/room/PlayerHand.tsx
+++ src/components/room/PlayerHand.tsx
@@ -1,11 +1,18 @@
 import { useState } from "react";
+import _ from "lodash";
+
 import { useRoom } from "../../context/RoomContext";
+import { useRestrictedPlayerData } from "../../context/RestrictedPlayerContext";
+
+import { Card } from "types/Card";
+
 import PlayingCard from "../PlayingCard";
 import Button from "../buttons/button";
-import { useRestrictedPlayerData } from "../../context/RestrictedPlayerContext";
 
 const PlayerHand = () => {
   const [selectedCard, setSelectedCard] = useState<number | null>(null);
+  const [sortMode, setSortMode] = useState<"default" | "suit" | "rank">("default");
+
   const { restrictedPlayer } = useRestrictedPlayerData();
   const { room } = useRoom();
 
@@ -36,16 +43,49 @@ const PlayerHand = () => {
 
   const handlePlayClick = () => {
     if (selectedCard !== null && isCurrentPlayer) {
-      // Perform the play action with the selected card
       console.log("Playing card:", restrictedPlayer?.cards[selectedCard]);
       setSelectedCard(null);
     }
   };
+
+  const sortCards = (cards: Card[]) => {
+    switch (sortMode) {
+      case "default":
+        return cards;
+      case "suit":
+        return _.chain(cards)
+          .groupBy("suit")
+          .map((group) => _.sortBy(group, (card) => card.value).reverse())
+          .flatten()
+          .value();
+      case "rank":
+        return _.sortBy(cards, (card) => card.value);
+      default:
+        return cards;
+    }
+  };
+
+  const handleSortClick = () => {
+    switch (sortMode) {
+      case "default":
+        setSortMode("suit");
+        break;
+      case "suit":
+        setSortMode("rank");
+        break;
+      case "rank":
+        setSortMode("default");
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
     <>
       <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2">
         <div className="flex justify-center -space-x-[12%]">
-          {restrictedPlayer?.cards.map((card, index) => (
+          {sortCards(restrictedPlayer?.cards || []).map((card, index) => (
             <PlayingCard
               key={index}
               card={card}
@@ -58,13 +98,17 @@ const PlayerHand = () => {
           ))}
         </div>
       </div>
-      {isTrickTakingPhase && isCurrentPlayer && selectedCard !== null && (
-        <div className="absolute right-5 bottom-5">
-          <Button size={2} theme="green" onClick={handlePlayClick}>
+      <div className="w-1/12 flex flex-col absolute right-5 bottom-4 gap-y-2">
+        {isTrickTakingPhase && isCurrentPlayer && selectedCard !== null && (
+          <Button size={1} theme="orange" onClick={handlePlayClick}>
             Play
           </Button>
-        </div>
-      )}
+        )}
+
+        <Button size={1} theme="yellow" onClick={handleSortClick}>
+          Sort
+        </Button>
+      </div>
     </>
   );
 };
