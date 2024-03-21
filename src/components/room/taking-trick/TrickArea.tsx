bac4d1d0974df31a4d43be56b73a006c605b875e
Ethan <ethanchen2009@hotmail.com>
2024-03-22 03:56:39 +0800

    Fix animation direction for cards

--- src/components/room/taking-trick/TrickArea.tsx
+++ src/components/room/taking-trick/TrickArea.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import PlayingCard from "../../PlayingCard";
-
 import { useAuth } from "../../../hooks/useAuth";
 import { useRoom } from "../../../context/RoomContext";
 
@@ -12,38 +11,56 @@ const TrickArea: React.FC = () => {
     return null;
   }
 
-  const currentPlayerPosition = room.players.find((player) => player.id === playerAccount.id)!.position as number;
+  const southPlayerPosition = room.players.find((player) => player.id === playerAccount.id)!.position as number;
+  const westPlayerPosition = (southPlayerPosition + 3) % 4;
+  const northPlayerPosition = (southPlayerPosition + 2) % 4;
+  const eastPlayerPosition = (southPlayerPosition + 1) % 4;
 
   const getCardAnimationClass = (playerPosition: number) => {
-    const relativePosition = (playerPosition - currentPlayerPosition + 4) % 4;
-    switch (relativePosition) {
-      case 0:
-        return "animate-south-card-fly-center";
-      case 1:
-        return "animate-east-card-fly-center";
-      case 2:
-        return "animate-north-card-fly-center";
-      case 3:
-        return "animate-west-card-fly-center";
+    switch (playerPosition) {
+      case southPlayerPosition:
+        return "animate-south-card-fly-center south-card";
+      case westPlayerPosition:
+        return "animate-west-card-fly-center west-card";
+      case northPlayerPosition:
+        return "animate-north-card-fly-center north-card";
+      case eastPlayerPosition:
+        return "animate-east-card-fly-center east-card";
+      default:
+        return "";
+    }
+  };
+
+  const getCardFinalPosition = (playerPosition: number) => {
+    switch (playerPosition) {
+      case southPlayerPosition:
+        return "bottom-1/3 left-1/2 transform -translate-x-1/2 translate-y-1/4";
+      case westPlayerPosition:
+        return "top-1/2 left-1/3 transform -translate-y-1/2 translate-x-1/4";
+      case northPlayerPosition:
+        return "top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/4";
+      case eastPlayerPosition:
+        return "top-1/2 right-1/3 transform -translate-y-1/2 -translate-x-1/4";
       default:
         return "";
     }
   };
 
   return (
-    <div className="flex justify-center items-center h-full">
-      <div className="relative w-full h-full">
-        {room.players.map((player) => (
-          <div
-            key={player.position}
-            className={`relative top-1/3 left-1/2 w-[100px] h-auto transition-all duration-500 ${getCardAnimationClass(
-              player.position as number
-            )}`}
-          >
-            {player.currentCardOnTable && <PlayingCard card={player.currentCardOnTable} />}
-          </div>
-        ))}
-      </div>
+    <div className="relative w-full h-full">
+      {room.players.map((player) => {
+        console.log({ player: player.position, southPlayerPosition });
+        return (
+          player.currentCardOnTable && (
+            <PlayingCard
+              card={player.currentCardOnTable}
+              className={`absolute transition-all duration-500 ${getCardAnimationClass(
+                player.position as number
+              )} ${getCardFinalPosition(player.position as number)}`}
+            />
+          )
+        );
+      })}
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,24 +11,30 @@ module.exports = {
       },
       keyframes: {
         "south-card-fly-center": {
-          "0%": { top: "50%", left: "50%", visibility: "hidden", transform: "translate(-50%, 0%)", opacity: 0 },
-          "10%": { left: "50%", opacity: 1, top: "70%", visibility: "visible", transform: "translate(-50%, 0%)" },
-          "100%": { top: "50%", left: "50%", opacity: 1, visibility: "visible", transform: "translate(-50%, 0%)" },
-        },
-        "north-card-fly-center": {
-          "0%": { top: "50%", left: "50%", transform: "translate(-50%, -100%)", visibility: "hidden", opacity: 0 },
-          "10%": { left: "50%", transform: "translate(-50%, -100%)", visibility: "visible", opacity: 1, top: "30%" },
-          "100%": { opacity: 1, top: "50%", left: "50%", transform: "translate(-50%, -100%)" },
-        },
-        "east-card-fly-center": {
-          "0%": { top: "50%", left: "50%", transform: "translate(50%, -50%)", visibility: "hidden", opacity: 0 },
-          "10%": { top: "50%", transform: "translate(50%, -50%)", visibility: "visible", opacity: 1, left: "70%" },
-          "100%": { top: "50%", transform: "translate(50%, -50%)", visibility: "visible", opacity: 1, left: "50%" },
+          "0%": {
+            top: "calc(100% - 33.33% + 20px)",
+            left: "50%",
+            transform: "translate(-50%, -50%) scale(1)",
+            opacity: 1,
+          },
+          "100%": { top: "calc(50% + 20px)", left: "50%", transform: "translate(-50%, -50%) scale(1)", opacity: 1 },
         },
         "west-card-fly-center": {
-          "0%": { top: "50%", left: "50%", transform: "translate(-150%, -50%)", opacity: 0, visibility: "hidden" },
-          "10%": { top: "50%", transform: "translate(-150%, -50%)", visibility: "visible", opacity: 1, left: "30%" },
-          "100%": { top: "50%", visibility: "visible", transform: "translate(-150%, -50%)", opacity: 1, left: "50%" },
+          "0%": { top: "50%", left: "calc(33.33% - 20px)", transform: "translate(-50%, -50%) scale(1)", opacity: 1 },
+          "100%": { top: "50%", left: "calc(50% - 20px)", transform: "translate(-50%, -50%) scale(1)", opacity: 1 },
+        },
+        "north-card-fly-center": {
+          "0%": { top: "calc(33.33% - 20px)", left: "50%", transform: "translate(-50%, -50%) scale(1)", opacity: 1 },
+          "100%": { top: "calc(50% - 20px)", left: "50%", transform: "translate(-50%, -50%) scale(1)", opacity: 1 },
+        },
+        "east-card-fly-center": {
+          "0%": {
+            top: "50%",
+            left: "calc(100% - 33.33% + 20px)",
+            transform: "translate(-50%, -50%) scale(1)",
+            opacity: 1,
+          },
+          "100%": { top: "50%", left: "calc(50% + 20px)", transform: "translate(-50%, -50%) scale(1)", opacity: 1 },
         },
       },
     },
