3bcead9e2ed2c67b70e68369111ae2d15f28bf69
Ethan <ethanchen2009@hotmail.com>
2024-08-08 16:20:41 +0800

    Fix mobile WaitingRoom PlayerPanel

--- src/features/waiting/components/PlayerBox.tsx
+++ src/features/waiting/components/PlayerBox.tsx
@@ -7,15 +7,15 @@ const PlayerBox = ({ player }: { player: PublicPlayer | undefined }) => {
   const isMe = player && playerAccount?.id === player.id;
 
   return (
-    <div className="text-3xs lg:text-base text-center aspect-square w-[12rem] min-w-[50px] min-h-[50px] m-2.5 bg-black bg-opacity-5 hover:bg-opacity-20 flex flex-col items-center justify-center mobile-portrait:h-[120px] mobile-portrait:w-[120px] mobile-landscape:w-[10vw]">
+    <div className="text-4xs min-w-[12vmax] min-h-[12vmax] h-[12vmax] w-[12vmax] md:text-xs bg-black bg-opacity-5 hover:bg-opacity-20 flex flex-col items-center justify-center">
       {player ? (
         <>
-          <div>
-            <span className="text-2xs md:text-base">{player.isHost ? "👑" : ""}</span>
-            <span className={`${isMe ? "text-yellow-400" : "text-white"}`}>{player.displayName}</span>
-          </div>
+          <span className={`${isMe ? "text-yellow-400" : "text-white"} text-center`}>
+            {player.isHost ? "👑" : ""} {player.displayName}
+          </span>
+
           <img
-            className="h-[5rem]"
+            className="h-[3rem]"
             src={avatarLookup[player.avatarID]}
             alt={player.displayName}
             style={{ imageRendering: "pixelated" }}
--- src/features/waiting/components/PlayerPanel.tsx
+++ src/features/waiting/components/PlayerPanel.tsx
@@ -12,15 +12,15 @@ const PlayerPanel = ({ room, className, ...props }: PlayerPanelProps) => {
   return (
     <div className={`bg-black/10 rounded-lg flex flex-col ${className}`} {...props}>
       <h4 className="font-bold p-4 h-[10%]">Players</h4>
-      <div className="h-[80%] px-4">
-        <div className="grid grid-rows-2 grid-cols-2 gap-1">
-          {[0, 1, 2, 3].map((index) => (
-            <div key={index} className="flex items-center justify-center">
-              <PlayerBox player={players[index]} />
-            </div>
-          ))}
-        </div>
+
+      <div className="h-[80%] w-full px-4 grid grid-rows-2 grid-cols-2">
+        {[0, 1, 2, 3].map((index) => (
+          <div key={index} className="h-full flex items-center justify-center">
+            <PlayerBox player={players[index]} />
+          </div>
+        ))}
       </div>
+
       <div className="h-[10%] px-2 mb-5">
         <RoomButtonPanel room={room} />
       </div>
