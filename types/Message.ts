134674c669c59c5ba8a7ac8abd6bddb0a5b8364c
Ethan <ethanchen2009@hotmail.com>
2024-08-09 21:32:55 +0800

    Move chat to use realtime database

--- types/Message.ts
+++ types/Message.ts
@@ -4,7 +4,10 @@ export interface Message {
   // Player info
   uid: string;
   playerName: string;
-
   text: string;
   createdAt: Timestamp;
+
+  type?: "chat" | "system";
+  title?: string; // Only for system messages
+  content?: unknown; // Only for system messages
 }
